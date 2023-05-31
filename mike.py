ee9acf74d952bc9196eaab40695855e952dee9b4
mikewenner <mikewenner121@gmail.com>
2023-05-30 21:44:10 -0400

    initial work on dfs for dashboard

---
+++ mike.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import streamlit as st
+# from openbb_terminal.core.plots.plotly_helper import OpenBBFigure, theme  # noqa: F401
+from openbb_terminal.sdk import openbb
+
+# pd.options.plotting.backend = "plotly"
+
