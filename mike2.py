44f4a5e60defa909641950e804370f7ccf6891a9
mikewenner <mikewenner121@gmail.com>
2023-06-08 10:24:33 -0400

    finished commodities, adding single stock

--- mike2.py
+++ mike2.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+from datetime import date, timedelta, datetime
+from dateutil.relativedelta import relativedelta
 from openbb_terminal.core.plots.plotly_helper import OpenBBFigure, theme  # noqa: F401
 from openbb_terminal.sdk import openbb
 
@@ -20,39 +22,42 @@ def color_negative_red(val):
         color = "green" if val > 0 else "red"
         return f"color: {color}"
     
+start = (date.today() - relativedelta(months=12)).strftime('%Y-%m-%d')
+end = (date.today() + timedelta(days=1)).strftime('%Y-%m-%d')
+
 #Create df for plotting    
 plot_onedf = openbb.economy.index(indices= ["dow_dji", "nasdaq", "sp500", "dow_djus", "russell2000", "nyse", "sp400", "cboe_vix"],
     interval = "1d",
-    start_date = "2022-05-30",
-    end_date = "2023-05-30",
+    start_date = start,
+    end_date = end,
     column = "Close",
     #returns: bool = False,
 )
 
 #combining ym & es into the index dataframe, they are not incorporated in economy.index pull
-sp_futs = openbb.futures.historical(symbols = "ES", start_date = "2022-05-30", end_date = "2023-05-30")
+sp_futs = openbb.futures.historical(symbols = "ES", start_date = start, end_date = end)
 sp_futs = sp_futs.drop(columns=["Open", "High", "Low", "Adj Close", "Volume"])
-dow_futs = openbb.futures.historical(symbols = "YM", start_date = "2022-05-30", end_date = "2023-05-30")
+dow_futs = openbb.futures.historical(symbols = "YM", start_date = start, end_date = end)
 dow_futs = dow_futs.drop(columns=["Open", "High", "Low", "Adj Close", "Volume"])
 
 #create commodities df for plotting
 symbols = ["cl", "bz", "gc", "si", "ng", "rb", "hg", "zc", "zw"]
-comm_df = openbb.futures.historical(symbols = symbols, start_date = "2022-05-30", end_date = "2023-05-30")
+comm_df = openbb.futures.historical(symbols = symbols, start_date = start, end_date = end)
 comm_df = comm_df.iloc[:,0:9]
-comm_df.columns = ["Brent Crude","Oil WTIC","Gold","Copper","Natural Gas","Gsoline RBOB","Silver","Corn","Wheat"]
+comm_df.columns = ["Brent Crude","Crude Oil","Gold","Copper","Natural Gas","Unleaded Gasoline","Silver","Corn","Wheat"]
 
 #Final df for plotting Indicies & commodities
 indicies_plot_df = pd.concat([plot_onedf, dow_futs, sp_futs, comm_df], axis=1)
 indicies_plot_df.columns = ["DJIA", "Nasdaq Composite", "S&P 500", "DJ Total Stock Market", "Russell 2000",
                             "NYSE Composite", "Barron's 400", "CBOE Volatility", "DJIA Futures", "S&P 500 Futures",
-                           "Brent Crude","Oil WTIC","Gold","Copper","Natural Gas","Gsoline RBOB","Silver","Corn","Wheat"
+                           "Brent Crude","Crude Oil","Gold","Copper","Natural Gas","Unleaded Gasoline","Silver","Corn","Wheat"
                            
                            ]
 
 
 #Streamlit
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_option('deprecation.showPyplotGlobalUse', False)  #this needed to remove warning when st.pyplot() called
 
 st.set_page_config(
     layout="wide",
@@ -66,7 +71,7 @@ image_streamlit = 'https://raw.githubusercontent.com/mikewenner/Streamlit/main/I
 
 index_list = ["DJIA", "Nasdaq Composite", "S&P 500", "DJ Total Stock Market", "Russell 2000",
                 "NYSE Composite", "Barron's 400", "CBOE Volatility", "DJIA Futures", "S&P 500 Futures",
-                "Brent Crude","Oil WTIC","Gold","Copper","Natural Gas","Gsoline RBOB","Silver","Corn","Wheat"
+                "Brent Crude","Crude Oil","Gold","Copper","Natural Gas","Unleaded Gasoline","Silver","Corn","Wheat"
                 ]
 
 #header
@@ -98,9 +103,19 @@ with col1:
         
 with col2:
     with st.container():
-        # st.subheader("Charts")
-        # indicies = st.selectbox(label = "Choose an index", options = index_list)
-        # title = st.write("Daily Close")
         selected_column = st.selectbox("Select Index", index_list)
         plt.plot(indicies_plot_df.index, indicies_plot_df[selected_column])
         st.pyplot()
+
+col1, col2, col3 = st.columns([2.5, 5, 1])
+with col2:
+        st.subheader("Calendar of Economic Events")
+        data = openbb.economy.events()
+        data = data.set_index(data.columns[0])
+        st.dataframe(data)
+
+st.subheader("Enter Your Favorite Stock Symbol")
+
+text_input = st.text_input("Symbol").upper()
+new_data = openbb.stocks.load(text_input)
+new_data
